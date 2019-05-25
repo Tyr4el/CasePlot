251150247c6dd913b98a4f2995691ed3c614ed5f
Nick Conklin <nconkl1@outlook.com>
2019-05-25 14:19:56 -0400

    Added plotting

--- plot.py
+++ plot.py
@@ -11,9 +11,11 @@ df.drop(['Case Number', 'Issue Summary', 'Account Name'], axis=1,
 
 # Convert the Opened Date column to datetime
 df['Opened Date'] = pd.to_datetime(df['Opened Date'])
+
 # Sort them by date
 df.sort_values(by=['Opened Date'], inplace=True, ascending=True)
 # Convert the Opened Date to a Day of Week
+# df['Day of Week'] = df.loc(df['Opened Date'].dt.dayofweek <= 4)
 df['Day of Week'] = df['Opened Date'].dt.dayofweek
 
 # Create the # of Cases column from the Opened Date
@@ -22,5 +24,14 @@ df['# of Cases'] = df.groupby('Opened Date')['Opened Date'].transform('count')
 # Now that we have our # of Cases counted up by day, let's drop the non-unique days
 df.drop_duplicates('Opened Date', inplace=True)
 
-df.plot(x='Opened Date', y='# of Cases', color='red')
+# Let's see what it looks like and plot it
+df.plot(x='Opened Date', y='# of Cases')
+
+# Gotta show it in the IDE!
 plt.show()
+# Save it
+plt.savefig('output.png')
+
+pd.set_option('display.max_rows', None)
+print(df)
+print(max(df["# of Cases"]))
