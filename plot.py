a960e2397f6d39ee595b81a9b4ac21d56da341df
Nick Conklin <nconkl1@outlook.com>
2019-05-26 09:07:43 -0400

    Update plot.py

--- plot.py
+++ plot.py
@@ -15,8 +15,8 @@ df['Opened Date'] = pd.to_datetime(df['Opened Date'])
 # Sort them by date
 df.sort_values(by=['Opened Date'], inplace=True, ascending=True)
 # Convert the Opened Date to a Day of Week
-# df['Day of Week'] = df.loc(df['Opened Date'].dt.dayofweek <= 4)
-df['Day of Week'] = df['Opened Date'].dt.dayofweek
+df['Day of Week'] = df.loc(df['Opened Date'].dt.dayofweek <= 4)
+# df['Day of Week'] = df['Opened Date'].dt.dayofweek
 
 # Create the # of Cases column from the Opened Date
 df['# of Cases'] = df.groupby('Opened Date')['Opened Date'].transform('count')
@@ -29,9 +29,13 @@ df.plot(x='Opened Date', y='# of Cases', figsize=(48, 8))
 # Save it
 plt.savefig('output.png')
 
+# Save the new CSV
+df.to_csv('Opened_Cases_RR_AllTime_Modified.csv', encoding='utf-8', index=False)
+
+# Random debugging shit
+pd.option_context('display.max_rows', None)
+print(df)
+print(len(df.index))
+
 # Gotta show it in the IDE!
 plt.show()
-
-pd.set_option('display.max_rows', None)
-print(df)
-print(max(df["# of Cases"]))
