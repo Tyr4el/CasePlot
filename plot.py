bc8b5927858c8f4a8616e71c573c221483a0fbe2
Nick Conklin <nconkl1@outlook.com>
2019-05-28 17:17:18 -0400

    Attempted to convert the timedelta to an int with days

--- plot.py
+++ plot.py
@@ -15,7 +15,7 @@ start_date = min(df['Opened Date'])
 
 # Calculate the days since the start date for each row
 df["Days Since Start Date"] = df['Opened Date'] - start_date
-
+df["Days Since Start Date"] = df["Days Since Start Date"].days
 # Convert the Opened Date to a Day of Week
 df['Day of Week'] = df['Opened Date'].dt.dayofweek
 df = df[df['Day of Week'] <= 4]
@@ -26,12 +26,14 @@ df['# of Cases'] = df.groupby('Opened Date')['Opened Date'].transform('count')
 # Now that we have our # of Cases counted up by day, let's drop the non-unique days
 df.drop_duplicates('Opened Date', inplace=True)
 
+df2 = df.copy()
+
 # Let's see what it looks like and plot it
-plot1 = df.plot(x='Opened Date', y='# of Cases')
-plot2 = df.plot(x='Days Since Start Date', y='# of Cases', kind='scatter')
+df.plot(x='Opened Date', y='# of Cases')
+df2.plot(x='Days Since Start Date', y='# of Cases', kind='scatter')
+
 # Save it
-plot1.savefig('output.png')
-plot2.savefig('output_scatter.png')
+plt.savefig('output.png')
 
 # Save the new CSV
 df.to_csv('Opened_Cases_RR_AllTime_Modified.csv', encoding='utf-8', index=False)
@@ -39,9 +41,7 @@ df.to_csv('Opened_Cases_RR_AllTime_Modified.csv', encoding='utf-8', index=False)
 # Random debugging shit
 pd.option_context('display.max_rows', None, 'display.max_columns', None)
 print(df)
+df.info()
 
 # Gotta show it in the IDE!
-plot1.show()
-plot2.show()
-
-print(df.columns)
+plt.show()
